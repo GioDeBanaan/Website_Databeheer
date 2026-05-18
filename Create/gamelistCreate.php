88752b27fb5854b7e689e861526aee0d5d154452
kai112lol <kai@hiraki.nl>
2026-05-18 11:44:57 +0200

    Engeleses

--- Create/gamelistCreate.php
+++ Create/gamelistCreate.php
@@ -1,14 +1,14 @@
 <html>
     <head>
-        <title>voeg Game toe</title>
+        <title>Add game</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-        <h1>voeg nieuwe game toe</h1>
+        <h1>Add new game</h1>
         <form method="post" action="../Pages/gamelist.php?action=store">
             <?php include __DIR__ . "/../Models/config.php"; 
             include __DIR__ . "/gamelistForm.php"; ?>
-        <button type="submit" class="btn btn-primary">voeg game toe</button>
+        <button type="submit" class="btn btn-primary">Add game</button>
     </form>
     </body>
 </html>
