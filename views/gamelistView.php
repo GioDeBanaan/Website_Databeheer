9f6c5f79fdcd68729aebe9e9884cb16a2d70e58f
kai112lol <kai@hiraki.nl>
2026-05-21 15:02:10 +0200

    super smegma

--- views/gamelistView.php
+++ views/gamelistView.php
@@ -54,7 +54,7 @@
                     <td class="text-nowrap"><?= htmlspecialchars($game["released_at"]) ?></td>
                     <td><?= htmlspecialchars($game["genre_names"] ?? $game["genre_name"] ?? '') ?></td>
                     <td><?= htmlspecialchars($game["platform_names"] ?? $game["platform_name"] ?? '') ?></td>
-                    <td><?= is_numeric($game["personal_rating"]) ? number_format((float)$game["personal_rating"], 1, '.', '') : htmlspecialchars($game["personal_rating"]) ?></td>
+                    <td>卍<?= is_numeric($game["personal_rating"]) ? number_format((float)$game["personal_rating"], 1, '.', '') : htmlspecialchars($game["personal_rating"]) ?></td>
                     <td><?= htmlspecialchars($game["rawg_rating"]) ?></td>
                     <td><?= htmlspecialchars($game["created_at"]) ?></td>
                     <td><?= htmlspecialchars($game["updated_at"]) ?></td>
